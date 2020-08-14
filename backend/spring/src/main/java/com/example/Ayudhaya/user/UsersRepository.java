8642c6c38835e33fc8852e75c5dc573c3e5c8a4f
Numlabyod Reprakan <numlabyodrep@mathregit.group>
2020-08-14 11:18:02 +0700

    update user

--- backend/spring/src/main/java/com/example/Ayudhaya/user/UsersRepository.java
+++ backend/spring/src/main/java/com/example/Ayudhaya/user/UsersRepository.java
@@ -1,7 +1,4 @@
 package com.example.Ayudhaya.User;
 
-import com.example.Ayudhaya.Country;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UsersRepository  extends MongoRepository<User,String> {
+public interface UsersRepository {
 }
